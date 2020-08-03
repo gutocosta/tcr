b02c88b38a2ab331c9ead2156788c224eea1c935
Augusto da Silva Costa <augusto.costa@instruct.com.br>
2020-08-03 10:32:31 -0300

    working

---
+++ src/fatorial.py
@@ -0,0 +1,6 @@
+def fatorial(x):
+    fatorial = 1
+    while (x > 1):
+        fatorial = x * fatorial
+        x = x - 1
+    return fatorial
--- src/fatorial_test.py
+++ src/fatorial_test.py
@@ -1,2 +1,4 @@
-import pytest
+from fatorial import fatorial
 
+def test_fatorial_3():
+    assert fatorial(3) == 6
